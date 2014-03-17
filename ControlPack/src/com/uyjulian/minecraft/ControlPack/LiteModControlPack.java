2c115fb950f8cbc29ea4bfa3082219843c6d7b57
uyjulian <uyjulian@gmail.com>
2014-03-17 14:36:18 -0500

    Add Litemod

---
+++ ControlPack/src/com/uyjulian/minecraft/ControlPack/LiteModControlPack.java
@@ -0,0 +1,34 @@
+package com.uyjulian.minecraft.ControlPack;
+
+import java.io.File;
+
+import com.mumfrey.liteloader.LiteMod;
+
+public class LiteModControlPack implements LiteMod {
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void init(File configPath) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void upgradeSettings(String version, File configPath,
+			File oldConfigPath) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
